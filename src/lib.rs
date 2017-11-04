5fc1e777f767c66b17978ff79dbad3ffb0d1ccdb
Dimitri Merejkowsky <d.merej@gmail.com>
2017-11-04 16:14:12 +0100

    Implement case sensitive grep

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,44 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
-    println!("Qrery: {}", config.query);
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
+
+    let results = search(&config.query, &contents);
+    for result in results {
+        print!("{}\n", result);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = " \
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
 }
