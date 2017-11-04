8752fe6aa621936fc8a73febcf48786af8178147
Dimitri Merejkowsky <d.merej@gmail.com>
2017-11-04 15:30:19 +0100

    Implement `cat`-like stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    let mut f = File::open(filename).expect("File not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Could not read from file");
+    print!("With text:\n{}", contents);
 }
